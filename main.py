9e3ba9d8b2f6a6d8516e66ac5a04239f882c2e9c
tanbushi <tanbushi@qq.com>
2024-05-21 21:38:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,44 @@ app = FastAPI()
 
 import requests, json, os
 
+is_koyeb = True
+
+if not is_koyeb:
+##############################
+# 下面为 .env 文件模式读取变量
+    from dotenv import load_dotenv
+# 加载 .env 文件
+    load_dotenv()
+##############################
+
+model="gemini-1.5-pro-latest"
+entrypoint="https://generativelanguage.googleapis.com/v1beta"
 
 @app.get("/")
 def read_root():
-    return {"Hello": os.environ.get('testenv')}
+    if is_koyeb:
+        apikey = os.environ.get('apikey') # 读取环境变量 koyeb 环境变量模式
+    else:
+        apikey = os.getenv('apikey') # 读取环境变量 .env 模式
 
+    url=f'{entrypoint}/models/{model}:generateContent?key={apikey}'
+    headers = {'Content-Type': 'application/json'}
+    
+    data = {
+        'contents': [
+            {
+                'parts': [
+                    {
+                        'text': '你好'
+                        }
+                    ]
+                }
+            ]
+        }
+ 
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-    
