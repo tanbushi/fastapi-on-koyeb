6641a18f2d78f4f4414855fcf25dcb330d2fd9de
tanbushi <tanbushi@qq.com>
2024-05-21 23:02:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 import requests, json, os
 
-is_koyeb = True
+is_koyeb = False
 
 if not is_koyeb:
 ##############################
@@ -18,33 +18,52 @@ if not is_koyeb:
 
 model="gemini-1.5-pro-latest"
 entrypoint="https://generativelanguage.googleapis.com/v1beta"
+if is_koyeb:
+    apikey = os.environ.get('apikey') # 读取环境变量 koyeb 环境变量模式
+else:
+    apikey = os.getenv('apikey') # 读取环境变量 .env 模式
+url=f'{entrypoint}/models/{model}:generateContent?key={apikey}'
+headers = {'Content-Type': 'application/json'}
 
 @app.get("/")
 def read_root():
-    if is_koyeb:
-        apikey = os.environ.get('apikey') # 读取环境变量 koyeb 环境变量模式
-    else:
-        apikey = os.getenv('apikey') # 读取环境变量 .env 模式
+    return {"Hello": "World"}
 
-    url=f'{entrypoint}/models/{model}:generateContent?key={apikey}'
-    print(url)
-    headers = {'Content-Type': 'application/json'}
-    
+    # data = {
+    #     'contents': [
+    #         {
+    #             'parts': [
+    #                 {
+    #                     'text': '你好'
+    #                     }
+    #                 ]
+    #             }
+    #         ]
+    #     }
+ 
+    # response = requests.post(url, headers=headers, data=json.dumps(data))
+    # return response.json()
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/sendtext/{text}")
+def read_item(text: str):
+    return send_text(text)
+
+def send_text(text):
     data = {
         'contents': [
             {
                 'parts': [
                     {
-                        'text': '你好'
+                        'text': text
                         }
                     ]
                 }
             ]
         }
- 
+    return data
     response = requests.post(url, headers=headers, data=json.dumps(data))
-    return response.json()
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return response.json()  
