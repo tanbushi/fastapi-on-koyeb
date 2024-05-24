1dd1c0c939fbed871dd59f673415ffa84d770cf9
檀布施 <tanbushi@qq.com>
2024-05-24 11:12:43 +0800

    main

--- main.py
+++ main.py
@@ -54,7 +54,11 @@ def read_item(text: str):
 
 @app.post("/items/")
 def create_item(item:dict):
-    return send_text(item['q'])
+    try:
+        q=item['q']
+    except:
+        return "q not found"
+    return send_text(q)
 
 def send_text(text):
     data = {
@@ -68,7 +72,10 @@ def send_text(text):
                 }
             ]
         }
-    # return data
+    # response = {"candidates":[{"content":{"parts":[{"text":"你好呀！ 很高兴见到你！ 你想聊些什么呢？ \n"}],"role":"model"},"finishReason":"STOP","index":0,"safetyRatings":[{"category":"HARM_CATEGORY_SEXUALLY_EXPLICIT","probability":"NEGLIGIBLE"},{"category":"HARM_CATEGORY_HATE_SPEECH","probability":"NEGLIGIBLE"},{"category":"HARM_CATEGORY_HARASSMENT","probability":"NEGLIGIBLE"},{"category":"HARM_CATEGORY_DANGEROUS_CONTENT","probability":"NEGLIGIBLE"}]}],"usageMetadata":{"promptTokenCount":2,"candidatesTokenCount":15,"totalTokenCount":17}}
+    
     response = requests.post(url, headers=headers, data=json.dumps(data))
-    return response.json()  
+    response_json = response.json()
+    return response_json['candidates'][0]['content']['parts'][0]['text'] 
+
 
